d7cb8b3d7553204748bf4ffc026698223a39c99e
haducluonghnk2023 <haducluongdz@gmail.com>
2024-08-12 22:19:40 +0700

    add

---
+++ Session01/my-app/app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Companyy from "@/pages/companyy";
+import Button from "@/components/Button";
+
+export default function Page() {
+  return (
+    <div>
+      <div>Họ và tên: Hạ Đức Lương</div>
+      <Companyy />
+      <Button />
+    </div>
+  );
+}
